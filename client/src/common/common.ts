0aaa1be89bf7caae5aa14ab6e99c33f685806623
Piotr Szymaniec <traf@angrenost.org>
2023-01-22 19:38:31 +0100

    feat: change dummy car names

--- client/src/common/common.ts
+++ client/src/common/common.ts
@@ -1,8 +1,44 @@
 import ICar from "./ICar"
-function generateName():string {
-  //dummy values
-  const carPrefixName = "ABCDEFGHIJKLMNOPRSTUW".split("")
-  const carSufixName = "1234567890".split("")
+function generateName():string {  
+  const carPrefixName = [
+    'Citroen',
+    'Renault',
+    'Fiat',
+    'Volksvagen',
+    'Mercedes',
+    'Rolce-Roys',
+    'Aston Martin',
+    'Suzuki',
+    'Deu',
+    'Lancia',
+    'Hyundai',
+    'Honda',
+    'Nissan',
+    'Mazda',
+    'Subaru',
+    'Lexus',
+    'Polonaz'
+  ]
+  
+  const carSufixName = [
+  'Sedan',
+  'Toro',
+  'Viper',
+  'Fire',
+  'Indiana',
+  'Focus',
+  'GT',
+  'Sport',
+  'Turbo',
+  'Combi',
+  'Impreza',
+  'Gold',
+  'Gamma',
+  'Alfa',
+  'Sigma',
+  'Silver',
+  'Omega'
+  ]
   return `${carPrefixName[Math.floor(Math.random()*carPrefixName.length)]} ${carSufixName[Math.floor(Math.random()*carSufixName.length)]}`
 }
 function generateColor():string {
